b9b061d3639da896df5433276782d6d50142df45
vinsontandraa <vtandra7@gmail.com>
2019-02-23 21:59:29 +0700

    perbaikin home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\category;
 
 class HomeController extends Controller
 {
@@ -23,8 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-
-        return view('home');
+        $category = category::all();
+      return view('home' , compact(['category']));
     }
 
 }
