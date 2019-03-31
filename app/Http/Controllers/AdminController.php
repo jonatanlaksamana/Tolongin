eef33ca4abf10ed2799e03754b1138d4c8b46605
Rizky Alwan <alwanrizky>
2019-03-31 15:51:59 +0700

    test

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,23 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
 
+use App\User;
 use App\Order;
 use DB;
-use Illuminate\Support\Facades\Auth;
-
-
+//alw
+use App\Request;
 
 class AdminController extends Controller
 {
     //
-
-
-
     public function index(){
-
         $User=User::all();
         $Order=Order::all();
         $joinTable=DB::table('requests')
@@ -26,37 +20,33 @@ class AdminController extends Controller
             $join->on('requests.idClient', '=', 'orders.idPemberiJasa');
         })
         ->get();
-        return view('Admin.AdminPanel', compact('User','Order','joinTable'));
+        //alw
+        $requests = DB::table('requests')
+            ->join('orders', 'users.id', '=', 'contacts.user_id')
+            ->join('orders', 'users.id', '=', 'orders.user_id')
+            ->select('users.*', 'contacts.phone', 'orders.price')
+            ->get();
 
+        return view('Admin.AdminPanel', compact('User','Order','joinTable'));
     }
     public function chart(){
-        $user = User::find(Auth::id());
-        return view('Admin/chart', compact('user') );
+return view('Admin/chart');
 
     }
     public function tables(){
-        $user = User::find(Auth::id());
 
-      return view('Admin.table', compact('user'));
+      return view('Admin/table');
     }
+    public function adminpanel(){
 
+      return view('Admin/AdminPanel');
+    }
     public function form(){
-        $user = User::find(Auth::id());
 
-      return view('Admin/form', compact('user'));
+      return view('Admin/form');
     }
     public function map(){
-        $user = User::find(Auth::id());
-        return view ('Admin/map', compact('user'));
+return view ('Admin/map');
 
     }
-    public function testimoni(){
-        $user = User::find(Auth::id());
-        $testimoni = DB::table('testimonis')->leftJoin('users', 'testimonis.idClient', '=', 'users.id')->get();
-        return view ('Admin.testimoni', compact('user' , 'testimoni'));
-
-    }
-
-
-
 }
