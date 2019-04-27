a8d2cf98a2b6da78b612dd00e0f3b3c1cbfa53d8
laraimut <larasoctaa@gmail.com>
2019-04-27 09:32:30 +0700

    baru

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="hero-wrap hero-bread" style="background-image: url('images/register.png');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact</span></p>
+            <h1 class="mb-0 bread">Contact Us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="nabrak">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -89,5 +101,7 @@
             </div>
         </div>
     </div>
+</div>
+<div class="nabrak"> </div>
 </div>
 @endsection
