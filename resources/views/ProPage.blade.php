efe8bba8e0ae295dcebf43a1d22c6ba8ee6b39ff
laraimut <larasoctaa@gmail.com>
2019-04-27 13:42:00 +0700

    finish profile

---
+++ resources/views/ProPage.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts/app')
+
+@section('content')
+<div class="nabrak2"></div>
+<div class="hero-wrap hero-bread" style="background-image: url('images/daftarjasa.png');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact</span></p>
+            <h1 class="mb-0 bread">Contact Us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  
+@endsection
