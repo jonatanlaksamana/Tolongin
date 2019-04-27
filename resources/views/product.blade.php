7f2a59f77297658c213ed6ecdd63fe526ff47ecf
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2019-04-27 14:57:37 +0700

    we only need payment gateway now

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -26,13 +26,9 @@
                         <img class="pic-1" src="{{asset('storage/' . $jas->image)}}">
                         <img class="pic-2" src="{{asset('storage/' . $jas->image)}}">
                     </a>
-                    <ul class="social">
-                        <li><a href="{{ route('productzoom') }}"data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="{{ route('productzoom') }}"data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
-                        <li><a href="{{ route('productzoom') }}"data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                    <span class="product-new-label">New</span>
-                    <span class="product-discount-label">-10%</span>
+
+{{--                    <span class="product-new-label">New</span>--}}
+{{--                    <span class="product-discount-label">-10%</span>--}}
                 </div>
                 <div class="product-content">
                     <h3 class="title"><a href="#">{{$jas->jasaName}}</a></h3>
@@ -40,13 +36,17 @@
                         Rp {{$jas->harga}}
                       
                     </div>
-                    <a class="add-to-cart" href="">ADD TO CART</a>
+                    <form action="{{url('/addcart/' . $jas->id)}}" method="post">
+                        @csrf
+                        <button class="btn btn-success"> Add To Cart</button>
+                    </form>
                 </div>
             </div>
         </div>
             @endforeach
 
-        {{ $jasa->links() }}
+
 </div>
+            {{ $jasa->links() }}
 </div>
 @endsection
