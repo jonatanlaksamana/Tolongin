ddad67dedec6507f3d22867fed097872ce1f5269
laraimut <larasoctaa@gmail.com>
2019-04-27 13:24:41 +0700

    penambahan profile baru

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,7 +144,7 @@
     	<div class="container">
 				<div class="row justify-content-center mb-3 pb-3">
           <div class="col-md-12 heading-section text-center ftco-animate">
-            <h2 class="mb-4">Products</h2>
+            <h2 class="mb-4">Jasa</h2>
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
           </div>
         </div>   		
@@ -153,15 +153,15 @@
     		<div class="row">
     			<div class="col-sm col-md-6 col-lg ftco-animate">
     				<div class="product">
-    					<a href="#" class="img-prod"><img class="img-fluid" src="images/produk1.jpeg" alt="Colorlib Template">
+    					<a href="#" class="img-prod"><img class="img-fluid" src="images/produk3.jpeg" alt="Colorlib Template">
     						<span class="status">30%</span>
     						<div class="overlay"></div>
     					</a>
     					<div class="text py-3 px-3">
-    						<h3><a href="#">Floral Jackquard Pullover</a></h3>
+    						<h3><a href="#">Products Photography</a></h3>
     						<div class="d-flex">
     							<div class="pricing">
-		    						<p class="price"><span class="mr-2 price-dc">$120.00</span><span class="price-sale">$80.00</span></p>
+		    						<p class="price"><span class="price-sale">Rp 8.000.000</span></p>
 		    					</div>
 		    					<div class="rating">
 	    							<p class="text-right">
@@ -186,10 +186,10 @@
 	    					<div class="overlay"></div>
 	    				</a>
     					<div class="text py-3 px-3">
-    						<h3><a href="#">Floral Jackquard Pullover</a></h3>
+    						<h3><a href="#">Product Branding Design</a></h3>
     						<div class="d-flex">
     							<div class="pricing">
-		    						<p class="price"><span>$120.00</span></p>
+		    						<p class="price"><span>Rp 5.000.000</span></p>
 		    					</div>
 		    					<div class="rating">
 	    							<p class="text-right">
@@ -214,10 +214,10 @@
     						<div class="overlay"></div>
     					</a>
     					<div class="text py-3 px-3">
-    						<h3><a href="#">Floral Jackquard Pullover</a></h3>
+    						<h3><a href="#">Packaging Design</a></h3>
     						<div class="d-flex">
     							<div class="pricing">
-		    						<p class="price"><span>$120.00</span></p>
+		    						<p class="price"><span>Rp 500.000</span></p>
 		    					</div>
 		    					<div class="rating">
 	    							<p class="text-right">
@@ -242,10 +242,10 @@
     						<div class="overlay"></div>
     					</a>
     					<div class="text py-3 px-3">
-    						<h3><a href="#">Floral Jackquard Pullover</a></h3>
+    						<h3><a href="#">Food Photography</a></h3>
     						<div class="d-flex">
     							<div class="pricing">
-		    						<p class="price"><span>$120.00</span></p>
+		    						<p class="price"><span>Rp 3.000.000</span></p>
 		    					</div>
 		    					<div class="rating">
 	    							<p class="text-right">
